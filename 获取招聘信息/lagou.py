ce803aa589133bb319852d6572b24f40368aa255
03-Winter <1487262758@qq.com>
2023-11-17 11:57:11 +0800

    反爬虫失败

---
+++ 获取招聘信息/lagou.py
@@ -0,0 +1,7 @@
+import requests
+url='https://www.lagou.com/shenzhen-zhaopin/Python/?labelWords=label'
+data={
+
+}
+headers={
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.69'}
